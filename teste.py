d6cb330a144b7bd1691987a9f9eef964bb876971
Anne Caroline <carolaragao94@gmail.com>
2023-03-16 17:41:03 -0300

    class conta e cliente

---
+++ teste.py
@@ -0,0 +1,11 @@
+from models.cliente import Cliente
+from models.conta import Conta
+
+felicity: Cliente = Cliente('Felicity Jones', 'felicity@gmail.com', '123-456-789-01', '02/09/1980')
+angelina: Cliente = Cliente('Angelina Jolie', 'angelina@gmail.com', '321-654-789-01', '08/07/1978')
+
+contaf: Conta = Conta(felicity)
+contaa: Conta = Conta(angelina)
+
+print(contaf)
+print(contaa)
