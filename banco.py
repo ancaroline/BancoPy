2ae2721d2cf441df3e15185ee199ce6589189815
Anne Caroline <carolaragao94@gmail.com>
2023-03-16 18:02:22 -0300

    função criar conta

--- banco.py
+++ banco.py
@@ -12,11 +12,57 @@ def main() -> None:
 
 
 def menu() -> None:
-    pass
+    print('============================')
+    print('=========== ATM ============')
+    print('========== Bank ============')
+
+    print('Selecione uma opção no menu: ')
+    print('1 - Criar conta')
+    print('2 - Efetuar saque')
+    print('3 - Efetuar depósito')
+    print('4 - Efetuar transferência')
+    print('5 - Listar contas')
+    print('6 - Sair do sistema')
+
+    opcao: int = int(input())
+
+    if opcao == 1:
+        criar_conta()
+    elif opcao == 2:
+        efetuar_saque()
+    elif opcao == 3:
+        efetuar_deposito()
+    elif opcao == 4:
+        efetuar_transferencia()
+    elif opcao == 5:
+        listar_contas()
+    elif opcao == 6:
+        print('Volte sempre.')
+        sleep(2)
+        exit(0)
+    else:
+        print('Opção inválida.')
+        sleep(2)
+        menu()
 
 
 def criar_conta() -> None:
-    pass
+    print('Informe os dados do cliente: ')
+    nome: str = input('Nome do cliente: ')
+    email: str = input('E-mail do cliente: ')
+    cpf: str = input('CPF do cliente: ')
+    data_nascimento: str = input('Data de nascimento do cliente: ')
+
+    cliente: Cliente = Cliente(nome, email, cpf, data_nascimento)
+    conta: Conta = Conta(cliente)
+    contas.append(conta)
+
+    print('Conta criada com sucesso.')
+    print('Dados da conta: ')
+    print('----------------')
+    print(conta)
+    sleep(2)
+    menu()
 
 
 def efetuar_saque() -> None:
@@ -24,6 +70,10 @@ def efetuar_saque() -> None:
 
 
 def efetuar_deposito() -> None:
+    pass
+
+
+def efetuar_transferencia() -> None:
     pass
 
 
